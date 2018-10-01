1cf839314f16643be9953b0bbd04075bebc80f0c
SNUC <9119124@studentmail.ul.ie>
2018-10-01 07:43:38 -0500

    Finished Tic-Tac_Toe Controller

--- TicTacToe/app/src/main/java/ie/ul/a9119124/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/ie/ul/a9119124/tictactoe/MainActivity.java
@@ -2,14 +2,16 @@ package ie.ul.a9119124.tictactoe;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private TicTacToeGame mGame;
+    private TextView mGameStateTextView;
+    private Button[] mButtons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,19 +19,51 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mGame = new TicTacToeGame(this);
+        mGameStateTextView = findViewById(R.id.game_state_text_view);
+        mButtons = new Button[TicTacToeGame.NUM_SQUARES];
+        mButtons[0] = findViewById(R.id.button0);
+        mButtons[1] = findViewById(R.id.button1);
+        mButtons[2] = findViewById(R.id.button2);
+        mButtons[3] = findViewById(R.id.button3);
+        mButtons[4] = findViewById(R.id.button4);
+        mButtons[5] = findViewById(R.id.button5);
+        mButtons[6] = findViewById(R.id.button6);
+        mButtons[7] = findViewById(R.id.button7);
+        mButtons[8] = findViewById(R.id.button8);
+        updateView();
 
     }
 
     public void pressedSquare(View view) {
         String tagAsStr = view.getTag().toString();
         int tagAsInt = Integer.parseInt(tagAsStr);
-        Log.d("TTT", "You pressed index " + tagAsInt);
 
-        Toast.makeText(this, "You pressed index " + tagAsInt, Toast.LENGTH_SHORT).show();
+        //Debug code
+        //Log.d("TTT", "You pressed index " + tagAsInt);
+        //Toast.makeText(this, "You pressed index " + tagAsInt, Toast.LENGTH_SHORT).show();
+
+        mGame.pressedButtonAtIndex(tagAsInt);
+
+        updateView();
+
+
+
+
+    }
+
+    private void updateView() {
+        mGameStateTextView.setText(mGame.stringForGameState());
+
+        for(int i = 0; i < TicTacToeGame.NUM_SQUARES; i++ ) {
+            mButtons[i].setText(mGame.stringForButtonAtIndex(i));
+        }
     }
 
     public void pressedNewGame(View view) {
-        Toast.makeText(this, "New Game", Toast.LENGTH_SHORT).show();
+        //Debug code
+        //Toast.makeText(this, "New Game", Toast.LENGTH_SHORT).show();
+        mGame = new TicTacToeGame(this);
+        updateView();
     }
 
 }
